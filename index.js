18e5d2caa4ba7abf48be4669570e35401839d24e
Arabella <127732263+ArabellaJordan@users.noreply.github.com>
2023-07-31 04:46:49 +0800

    bugs fix

--- index.js
+++ index.js
@@ -3,25 +3,14 @@ const modeButton = document.querySelector(".mode-button");
 const buttons = document.querySelectorAll("button");
 const navigationTexts = document.querySelectorAll("nav ul li");
 const svg = document.querySelectorAll("svg");
+const width = window.innerWidth;
+
 
 modeButton.addEventListener("click", function(){
     document.querySelector("body").classList.toggle("lightMode");
     document.querySelector(".intro h1").classList.toggle("lightMode");
     document.querySelector(".picture").classList.toggle("lightMode");
-    if (document.querySelector("body").classList.contains("lightMode")){
-        document.querySelector(".logo").src="./img/logo_black.png";
-        document.querySelector(".mode-button").src="./img/dark.png";
-    }
-    else{
-        document.querySelector(".logo").src="./img/logo_white.png";
-        document.querySelector(".mode-button").src="./img/light.png";
-    }
-    if (document.querySelector(".nav").classList.contains("active")){
-    document.querySelector(".nav.active").classList.toggle("lightMode");
-    }
-    else{
-        document.querySelector(".nav").classList.remove("lightMode");
-    }
+
     for(let i = 0; i< buttons.length; i++){
         buttons[i].classList.toggle("lightMode");
     }
@@ -31,6 +20,27 @@ modeButton.addEventListener("click", function(){
     for(let i = 0; i < svg.length; i++){
         svg[i].classList.toggle("lightMode");
     }
+
+    if (document.querySelector("body").classList.contains("lightMode")){
+        document.querySelector(".logo").src="./img/logo_black.png";
+        document.querySelector(".mode-button").src="./img/dark.png";
+    }
+    else{
+        document.querySelector(".logo").src="./img/logo_white.png";
+        document.querySelector(".mode-button").src="./img/light.png";
+    }
+    if (document.querySelector("body").classList.contains("lightMode")){
+        if (document.querySelector(".nav").classList.contains("active")){
+            document.querySelector(".nav.active").classList.add("lightMode");
+        }
+        else{
+            document.querySelector(".nav").classList.remove("lightMode");
+        }
+    }else{
+        document.querySelector(".nav.active").classList.remove("lightMode");
+    }
+
+    
 })
 
 //for dropdown navigation=========================================
